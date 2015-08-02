9f07e64ad88cee8966f562c791ffe007608663a2
Sam Mahdad <sammahdad@sams-mbp-2.attlocal.net>
2015-08-02 11:35:32 -0700

    Updated my tests and added an image file

--- ZipFile/java/com/smmahdad/filter/test/FileFilterTest.java
+++ ZipFile/java/com/smmahdad/filter/test/FileFilterTest.java
@@ -2,23 +2,39 @@ package com.smmahdad.filter.test;
 
 import com.smmahdad.filter.src.FileFilter;
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 import org.junit.Test;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
 
 
 public class FileFilterTest {
 
 	@Test
-	public void testWhenFileExists() {
+	public void testExistenceWhenFileExists() {
 		assertTrue(FileFilter.checkIfFileExists("testFiles/RandomText.txt") == 1);
 	}
 
 	@Test
-	public void testWhenFileNotFound() {
+	public void testExistenceWhenFileNotFound() {
 		assertTrue(FileFilter.checkIfFileExists("testFiles/RandomBlarjgaldkjfga.txt") == 0);
 	}
 
 	@Test
-	public void testWhenDirectory() {
+	public void testExistenceWhenDirectory() {
 		assertTrue(FileFilter.checkIfFileExists("testFiles") == 2);
 	}
+
+	@Test
+	public void testIfTextFileWhenItIs() {
+		assertTrue(FileFilter.checkIfFileIsATxtFileSimple("testFiles/RandomText.txt"));
+	}
+
+	@Test
+	public void testIfTextFileWhenItIsNot() {
+		assertFalse(FileFilter.checkIfFileIsATxtFileSimple("testFiles/FakeImage.jpg"));
+	}
+
 }
